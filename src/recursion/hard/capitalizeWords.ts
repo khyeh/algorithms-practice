80a048e6bdfcd552669c89031ab18b892d9b1e29
Kevin Yeh <kevin.yay@gmail.com>
2021-12-13 00:49:28 -0800

    Added capitalizeWords

---
+++ src/recursion/hard/capitalizeWords.ts
@@ -0,0 +1,14 @@
+// input: array of strings
+// output: array of string, but capitalized
+
+const capitalizeWords = (input: string[]): string[] => {
+    if (input.length === 1) {
+        return [input[0].toUpperCase()];
+    }
+    let result = capitalizeWords(input.slice(0, -1));
+    result.push(...capitalizeWords(input.slice(-1)));
+    return result;
+}
+
+let words = ['i', 'am', 'learning', 'recursion'];
+console.log(capitalizeWords(words)); // ['I', 'AM', 'LEARNING', 'RECURSION']
