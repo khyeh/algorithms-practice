e9460b292ea127fd1252129b6fd38f947c3e388c
Kevin Yeh <kevin.yay@gmail.com>
2021-12-22 00:20:12 -0800

    Add more basic singly linked list methods

--- src/linkedLists/singlyLinkedList.ts
+++ src/linkedLists/singlyLinkedList.ts
@@ -32,7 +32,6 @@ class SinglyLinkedList {
             this.head = newNode;
             this.tail = this.head;
         } else {
-            const newNode = new LinkedListNode(val);
             this.tail!.next = newNode;
             this.tail = newNode;
         }
@@ -61,6 +60,65 @@ class SinglyLinkedList {
         return current;
     }
 
+    /** Removes a node from the beginning (head node) of the linked list */
+    shift() {
+        if (!this.head) {
+            return undefined;
+        }
+        const currentHead = this.head;
+        this.head = currentHead.next;
+        this.length -= 1;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
+
+    /** Adds a new node to the beginning (head) of the linked list */
+    unshift(val: LinkedListNodeValue) {
+        const newNode = new LinkedListNode(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length += 1;
+        return this;
+    }
+
+    /** Returns the node the specified index */
+    get(index: number) {
+        if (!this.head || this.length < index) {
+            return null;
+        }
+        let current = this.head;
+        let i = index;
+        while (i > 0) {
+            // guard for TS because it cannot deduce that length check will guarantee
+            // current not to be null
+            if (current.next === null) {
+                return null;
+            }
+            current = current.next;
+            i--;
+        }
+        return current;
+    }
+
+    /** Change the value at specified index to specified value 
+     *  returns true if set is successful, false if not
+     */
+    set(index: number, val: LinkedListNodeValue) {
+        const locatedNode = this.get(index);
+        if (locatedNode) {
+            locatedNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
     traverse() {
         let current = this.head;
         while (current) {
@@ -74,6 +132,8 @@ const linkedList = new SinglyLinkedList();
 linkedList.push("hello")
 linkedList.push("world");
 linkedList.push(99);
-linkedList.pop();
+console.log(linkedList.set(2, "dog"));
+// linkedList.pop();
+// linkedList.shift();
 linkedList.traverse();
-console.log(linkedList)
+// console.log(linkedList)
